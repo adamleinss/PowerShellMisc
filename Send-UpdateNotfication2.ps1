6ac4ba413bbfb70c0f799297b229b7a714a5696e
Adam Leinss <35347500+adamleinss@users.noreply.github.com>
2020-09-25 22:36:33 -0500

    Add files via upload

--- Send-UpdateNotfication2.ps1
+++ Send-UpdateNotfication2.ps1
@@ -1,4 +1,4 @@
-﻿# Created by aleinss 1/30/2020
+﻿# Created by leinssam 1/30/2020
 # Used to send out e-mail notifications about servers that are patching
 
 # Find Patch Tuesday (2nd Tuesday of the month)
@@ -23,19 +23,19 @@ import-module (Join-Path $(Split-Path $ENV:SMS_ADMIN_UI_PATH) ConfigurationManag
 set-location ABC:
 CD ABC:
 
-# Patching Server Pilot – Auto Patch & Auto-Restart / Nightly maintenance window [7PM to 5AM]
+# Patching Server Pilot – Auto Patch & Auto-Restart / Nightly maintenance window [10PM to 5AM]
 
 # Set the maint window (should match maint window in SCCM)
 
-$patchStart = (Get-Date $date).AddDays($patchTuesday).AddHours(19)
-$patchEnd = (Get-Date $patchStart).AddHours(10)
+$patchStart = (Get-Date $date).AddDays($patchTuesday).AddHours(22)
+$patchEnd = (Get-Date $patchStart).AddHours(7)
 
 # Figure out what time it is
 
 $currentDate = Get-Date
 $startDT = $PatchStart.ToString("s")
 $endDT = $PatchEnd.ToString("s")
-$SendNotificationStartTime=$PatchStart.AddDays(-1)
+$SendNotificationStartTime=$PatchStart.AddDays(-1).AddHours(-14)
 $SendNotificationEndTime=$SendNotificationStartTime.AddMinutes(45)
 
 # If it's 24 hours before patching time, set $Sendmail to True, otherwise set to False
@@ -58,20 +58,22 @@ $serverpilotbr += "$_ <br>"
 
 # Send out e-mail.
 
- Send-MailMessage -From 'patchcalendar@acme.com' -To 'joe@acme.com' -Subject 'Server Pilot Patching' -Body "The following servers will be
- patched and restarted between $PatchStart and $PatchEnd : <br> $serverpilotbr" -SmtpServer '10.11.12.13' -BodyAsHtml
+Write-host "Sending out e-mail..."
+
+ Send-MailMessage -From 'vm-cm-01@acme.com' -To 'patches@acme.com' -Subject 'Server Pilot Patching' -Body "The following servers will be
+ patched and restarted between $PatchStart and $PatchEnd .  Please ignore NAGIOS/PRTG alerts during the maintenance window for these servers.  <br> $serverpilotbr" -SmtpServer '127.0.0.1' -BodyAsHtml
  $SendEmail = "False"
 }
 
 # Patching All Test/Dev & VM-XXX/DP-XXX Odds/DC Odds – Auto-Patch & Auto-Restart / +4 days after Patch Tuesday which will be the 2nd or 3rd weekend of the month [Saturday 5PM to Sunday 5PM] – Auto-patch and Auto-restart
 
-$patchStart = (Get-Date $date).AddDays($patchTuesday + 4).AddHours(17)
-$patchEnd = (Get-Date $patchStart).AddDays(1)
+$patchStart = (Get-Date $date).AddDays($patchTuesday + 4).AddHours(22)
+$patchEnd = (Get-Date $patchStart).AddHours(7)
 
 $currentDate = Get-Date
 $startDT = $PatchStart.ToString("s")
 $endDT = $PatchEnd.ToString("s")
-$SendNotificationStartTime=$PatchStart.AddDays(-1)
+$SendNotificationStartTime=$PatchStart.AddDays(-1).AddHours(-14)
 $SendNotificationEndTime=$SendNotificationStartTime.AddMinutes(45)
 
 if(([DateTime]($currentDate) -ge $SendNotificationStartTime) -and ([DateTime]($currentDate) -le $SendNotificationEndTime )){$SendEmail = "True"}else{$SendEmail = "False"}
@@ -87,22 +89,22 @@ $serverplus4br += "$_ <br>"
 }
 
 
- Send-MailMessage -From 'patchcalendar@acme.com' -To 'joe@acme.com' -Subject 'Server Weekend Patching +4 days after Patch Tuesday' -Body "The following servers will be
- patched and restarted between $PatchStart and $PatchEnd : <br> $serverplus4br" -SmtpServer '10.11.12.13' -BodyAsHtml
+ Send-MailMessage -From 'vm-cm-01@acme.com' -To 'patches@acme.com' -Subject 'Server Weekend Patching +4 days after Patch Tuesday' -Body "The following servers will be
+ patched and restarted between $PatchStart and $PatchEnd . Please ignore NAGIOS/PRTG alerts during the maintenance window for these servers. <br> $serverplus4br" -SmtpServer '127.0.0.1' -BodyAsHtml
  $SendEmail = "False"
 
  }
 
  # _DC/CM (All) & VM-XXX/DP-XXX Evens - Auto-patch and Auto-restart
 
-$patchStart = (Get-Date $date).AddDays($patchTuesday + 11).AddHours(17)
-$patchEnd = (Get-Date $patchStart).AddDays(1)
+$patchStart = (Get-Date $date).AddDays($patchTuesday + 11).AddHours(22)
+$patchEnd = (Get-Date $patchStart).AddHours(7)
 
 
 $currentDate = Get-Date
 $startDT = $PatchStart.ToString("s")
 $endDT = $PatchEnd.ToString("s")
-$SendNotificationStartTime=$PatchStart.AddDays(-1)
+$SendNotificationStartTime=$PatchStart.AddDays(-1).AddHours(-14)
 $SendNotificationEndTime=$SendNotificationStartTime.AddMinutes(45)
 
 if(([DateTime]($currentDate) -ge $SendNotificationStartTime) -and ([DateTime]($currentDate) -le $SendNotificationEndTime )){$SendEmail = "True"}else{$SendEmail = "False"}
@@ -118,20 +120,20 @@ $serverplus11br += "$_ <br>"
 }
 
 
- Send-MailMessage -From 'patchcalendar@acme.com' -To 'joe@acme.com' -Subject 'Server Weekend Patching +11 days after Patch Tuesday' -Body "The following servers will be
- patched and restarted between $PatchStart and $PatchEnd : <br> $serverplus11br" -SmtpServer '10.11.12.13' -BodyAsHtml
+ Send-MailMessage -From 'vm-cm-01@acme.com' -To 'patches@acme.com' -Subject 'Server Weekend Patching +11 days after Patch Tuesday' -Body "The following servers will be
+ patched and restarted between $PatchStart and $PatchEnd . Please ignore NAGIOS/PRTG alerts during the maintenance window for these servers. <br> $serverplus11br" -SmtpServer '127.0.0.1' -BodyAsHtml
  $SendEmail = "False"
  }
  
  # _Production Servers – Auto-Patch & Auto-Restart
 
-$patchStart = (Get-Date $date).AddDays($patchTuesday + 18).AddHours(17)
-$patchEnd = (Get-Date $patchStart).AddDays(1)
+$patchStart = (Get-Date $date).AddDays($patchTuesday + 18).AddHours(22)
+$patchEnd = (Get-Date $patchStart).AddHours(7)
 
 $currentDate = Get-Date
 $startDT = $PatchStart.ToString("s")
 $endDT = $PatchEnd.ToString("s")
-$SendNotificationStartTime=$PatchStart.AddDays(-1)
+$SendNotificationStartTime=$PatchStart.AddDays(-2).AddHours(-14)
 $SendNotificationEndTime=$SendNotificationStartTime.AddMinutes(45)
 
 if(([DateTime]($currentDate) -ge $SendNotificationStartTime) -and ([DateTime]($currentDate) -le $SendNotificationEndTime )){$SendEmail = "True"}else{$SendEmail = "False"}
@@ -146,8 +148,8 @@ $serverplus18br += "$_ <br>"
 
 }
 
-Send-MailMessage -From 'patchcalendar@acme.com' -To 'joe@acme.com' -Subject 'Server Weekend Patching +18 days after Patch Tuesday' -Body "The following servers will be
-patched and restarted between $PatchStart and $PatchEnd : <br> $serverplus18br" -SmtpServer '10.11.12.13' -BodyAsHtml
+Send-MailMessage -From 'vm-cm-01@acme.com' -To 'patches@acme.com' -Subject 'Server Weekend Patching +18 days after Patch Tuesday' -Body "The following servers will be
+patched and restarted between $PatchStart and $PatchEnd . Please ignore NAGIOS/PRTG alerts during the maintenance window for these servers. <br> $serverplus18br" -SmtpServer '127.0.0.1' -BodyAsHtml
 $SendEmail = "False"
 }
 
